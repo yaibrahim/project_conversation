41992a86aa1593b4723c8f5ecaca5034ace45006
ibrahim <ibrahimazeem41@gmail.com>
2025-04-07 00:30:24 +0500

    final changes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,7 +1,13 @@
-content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './config/**/*.rb',
-]
+module.exports = {
+    content: [
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+      './app/views/**/*.{erb,haml,html,slim}',
+      './config/**/*.rb',
+      './public/*.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
